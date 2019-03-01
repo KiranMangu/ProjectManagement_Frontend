dbf1c28e85037355f26d7e19fb920c69026641ce
KiranMangu <kiranmohanm@gmail.com>
2019-03-01 19:18:09 +0530

    User Interface

--- src/app/user/view-user/view-user.component.spec.ts
+++ src/app/user/view-user/view-user.component.spec.ts
@@ -5,21 +5,35 @@ import { ViewUserComponent } from './view-user.component';
 describe('ViewUserComponent', () => {
   let component: ViewUserComponent;
   let fixture: ComponentFixture<ViewUserComponent>;
-
+  let compiled;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ViewUserComponent ]
+      declarations: [ViewUserComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ViewUserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain ReactiveForm properties', () => {
+    //TODO
+  });
+
+  it('should contain table control', () => {
+    expect(compiled.querySelector('table')).toBeTruthy();
+  });
+
+  it('should containt search controls', () => {
+    expect(compiled.querySelector('input[formControlName="searchField"] ')).toBeTruthy();
   });
 });
