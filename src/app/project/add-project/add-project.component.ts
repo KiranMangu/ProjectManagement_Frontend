bcf65a12d79fbdc375ab661555b5a74970915f2d
KiranMangu <kiranmohanm@gmail.com>
2019-03-13 20:49:25 +0530

    Project component

--- src/app/project/add-project/add-project.component.ts
+++ src/app/project/add-project/add-project.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { User } from '../../model/user.model';
+import { UserService } from '../../service/user.service';
 
 @Component({
   selector: 'app-add-project',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddProjectComponent implements OnInit {
 
-  constructor() { }
+  projectGroup: FormGroup;
+  constructor(private _fb: FormBuilder, private _usrSrv: UserService) { }
+  // dateRequired: boolean = false;
+  dateDisable: boolean = true;
 
   ngOnInit() {
+    this.projectGroup = this._fb.group({
+      projectName: ['', [Validators.required]],
+      dateRequired: [false], // Not neede for backend operations
+      startDate: [''],
+      endDate: [''],
+      priority: [0],
+      user: ['', { value: '', disabled: true }, [Validators.required]]
+    });
+  }
+
+  onSetDateChange(): void {
+    this.dateDisable = this.projectGroup.controls.dateRequired.value;
+    if (this.dateDisable) {
+      this.projectGroup.controls.startDate.setValue('');
+      this.projectGroup.controls.endDate.setValue('');
+    }
+    else {
+      var today = new Date()
+      this.projectGroup.controls.startDate.setValue(today);
+      today.setDate(today.getDate() + 1);
+      this.projectGroup.controls.endDate.setValue(today);
+    }
+    // console.log('11' + this.projectGroup.controls.dateRequired.value);
+    // this.projectGroup.controls.dateRequired.setValue(!this.projectGroup.controls.dateRequired.value);
+    // console.log('22' + this.projectGroup.controls.dateRequired.value);
+  }
+
+  getUsers(): void {
+
+  }
+
+  addProject(): void {
+
+  }
+
+  resetControls(): void {
+    this.projectGroup.reset();
   }
 
 }
